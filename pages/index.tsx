5d2933b3068d7e5b9db1a15c8bc8595e99f85e1a
Matas555 <matas.jasonas@outlook.com>
2021-11-07 11:49:26 +0200

    initial layout and plot grid generation

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,36 +2,63 @@ import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-const Home: NextPage = () => (
-  <div className="flex flex-col items-center justify-center min-h-screen py-2">
-    <Head>
-      <title>T2 Farm</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home: NextPage = () => {
+  const generatedGrid = () => {
+    const grid = [];
+    for (let i = 0; i < 25; i += 1) {
+      grid.push(<div className="flex h-20 w-20 bg-green-200 items-center justify-center">{i}</div>);
+    }
+    return grid;
+  };
 
-    <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-      <h1 className="text-6xl font-bold">
-        Welcome to TokenToken farm
-      </h1>
-
-      <p className="mt-3 text-2xl">
-        It`s ahh... token ¯\_(ツ)_/¯
-      </p>
-
-      <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-        <div className="p-6 mt-6 text-left border w-full rounded-xl">
-          <h3 className="text-2xl font-bold">Graph here</h3>
-          <p className="mt-4 text-xl">
-            Yes veri candle, also many gren
-          </p>
+  return (
+    <div className="flex flex-col min-h-screen  bg-green-100">
+      <Head>
+        <title>T2 Farm</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="flex flex-row h-16">
+        <div className="flex w-1/3 justify-start items-center pt-2">
+          <h1 className="text-3xl font-bold pl-3">
+            TokenToken farm
+          </h1>
         </div>
-      </div>
-    </main>
-
-    <footer className="flex items-center justify-center w-full h-24 border-t">
-      Powered by... electricity ¯\_(ツ)_/¯
-    </footer>
-  </div>
-);
+        <div className="flex w-1/3 justify-center items-center pt-2 font-bold">
+          <div className="mx-2"><button className="font-bold" type="button">Fields</button></div>
+          <div className="mx-2"><button className="font-bold" type="button">Shop</button></div>
+          <div className="mx-2"><button className="font-bold" type="button">Barn</button></div>
+        </div>
+        <div className="flex w-1/3 items-bottom justify-end items-center pr-10">
+          <span className="font-bold">your address here</span>
+        </div>
+      </header>
+      <main className="flex flex-col items-center justify-top w-full h-full flex-1 px-20 mt-20 text-center">
+        <div className="mb-2">
+          <span>Farm field coordinates</span>
+        </div>
+        <div className="flex flex-row items-center justify-center">
+          <div className="mx-2">
+            <label htmlFor="coordinateX">
+              X:
+              <input id="coordinateX" name="coordinateX" className="input" type="number" />
+            </label>
+          </div>
+          <div className="mx-2">
+            <label htmlFor="coordinateY">
+              Y:
+              <input id="coordinateY" name="coordinateY" className="input" type="number" />
+            </label>
+          </div>
+        </div>
+        <div className="grid grid-cols-5 gap-1 mt-10">
+          { generatedGrid() }
+        </div>
+      </main>
+      <footer className="flex w-full h-12 items-center justify-center">
+        Powered by... electricity and tears ¯\_(ツ)_/¯
+      </footer>
+    </div>
+  );
+};
 
 export default Home;

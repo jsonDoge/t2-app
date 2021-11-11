fae06beff2e983e6ba0931a653f39c26e4f8283b
Matas555 <matas.jasonas@outlook.com>
2021-11-11 23:26:08 +0200

    cheap workaround not all tailwind classes loaded

--- pages/index.tsx
+++ pages/index.tsx
@@ -181,16 +181,17 @@ const Home: NextPage = () => {
       <div className="mt-10">
         <div className="grid grid-cols-12">
           <div className="grid-cols-1"></div>
-          <div className={`grid grid-cols-${gridXAxis.length} gap-1 col-span-11`}>
+          <div className={`grid gap-1 col-span-11 grid-cols-${gridXAxis.length}`}>
             { gridXAxis }
           </div>
         </div>
         <div className="grid grid-cols-12">
           <div className="grid col-span-1 grid-cols-1 gap-1">{ gridYAxis }</div>
-          <div className={`grid grid-cols-${gridXAxis.length} gap-1 col-span-11`}>
+          <div className={`grid gap-1 col-span-11 grid-cols-${gridXAxis.length}`}>
             { grid }
           </div>
         </div>
+        <div className="grid-cols-5 grid-cols-4" />
       </div>
       {isBuyPlotModalShown &&
         <Modal

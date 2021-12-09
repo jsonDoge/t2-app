ec9d3c8fec887f657e768ab5e41db25fc6230455
Matas555 <matas.jasonas@outlook.com>
2021-12-09 21:10:05 +0200

    fix: use suspense in ssr disabled component

--- components/background.tsx
+++ components/background.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Grid from './three/grid';
 
 const Background: React.FC<{}> = () => (
   <Canvas shadows className="min-h-screen w-screen">
-    <Grid />
+    <Suspense fallback={(<p>Loading</p>)}>
+      <Grid />
+    </Suspense>
   </Canvas>
 );
 

b2ac11f6948f24d4785967c94b0f0e7402009ac9
Matas555 <matas.jasonas@outlook.com>
2022-01-11 23:08:10 +0200

    fix: missed for update

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
 export const getAllCoordinatesAround = (x: number, y: number) => {
   const coordinates = [];
-  for (let dy = -2; dy < 3; dy += 1) {
-    for (let dx = -2; dx < 3; dx += 1) {
+  for (let dy = -3; dy < 4; dy += 1) {
+    for (let dx = -3; dx < 4; dx += 1) {
       const sumX = x + dx;
       const sumY = y + dy;
       if (sumX > 999 || sumY > 999 || sumX < 0 || sumY < 0) { continue; }

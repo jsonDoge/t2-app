a367e2fdff8eb0071abe61c2db3f82e0dbaad2d3
jsonDoge <matas.jasonas@protonmail.com>
2024-06-01 19:13:57 +0300

    feat: move blockCounter functionality to separate contextProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import 'tailwindcss/tailwind.css';
 import dynamic from 'next/dynamic';
 import WalletContextProvider from '../context/wallet';
 import GameContextProvider from '../context/game';
+import BlockchainContextProvider from '../context/blockchain';
 import ErrorContextProvider from '../context/error';
 import Layout from '../components/layout';
 import Spinner from '../components/spinner';
@@ -12,26 +13,28 @@ const Game = dynamic(() => import('../components/game'), { suspense: true, ssr: 
 
 function MyApp() {
   return (
-    <WalletContextProvider>
-      <GameContextProvider>
-        <>
-          <div className="absolute z-10 h-screen w-screen">
-            <Suspense
-              fallback={
-                <div className="flex items-center justify-center h-screen">
-                  <Spinner className="h-10 w-10" />
-                </div>
-              }
-            >
-              <Game />
-            </Suspense>
-          </div>
-          <ErrorContextProvider>
-            <Layout />
-          </ErrorContextProvider>
-        </>
-      </GameContextProvider>
-    </WalletContextProvider>
+    <BlockchainContextProvider>
+      <WalletContextProvider>
+        <GameContextProvider>
+          <>
+            <div className="absolute z-10 h-screen w-screen">
+              <Suspense
+                fallback={
+                  <div className="flex items-center justify-center h-screen">
+                    <Spinner className="h-10 w-10" />
+                  </div>
+                }
+              >
+                <Game />
+              </Suspense>
+            </div>
+            <ErrorContextProvider>
+              <Layout />
+            </ErrorContextProvider>
+          </>
+        </GameContextProvider>
+      </WalletContextProvider>
+    </BlockchainContextProvider>
   );
 }
 
